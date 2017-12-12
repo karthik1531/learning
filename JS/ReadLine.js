4b714c5bcd521df9494f3761e4682687a48bd184
Karthik Reddy <karthikreddylingareddy@gmail.com>
2017-12-12 22:55:19 +0530

    Add files via upload

---
+++ JS/ReadLine.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '192.168.1.10';
+const port = '2000';
+
+fs.readFile('test.txt', (err,text) => {
+	if(err){
+		console.log(err);
+	}
+	const server= http.createServer((req,res) => {
+		res.statusCode = 200;
+		res.setHeader('Content-type','textplain');
+		res.write(text);
+		res.end();
+	});
+	server.listen(port,hostname, () => {
+		console.log('Server started on port '+port);
+	});
+});
+
