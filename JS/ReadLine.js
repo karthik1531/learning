cd46aeaf219e2b1d286b166ded05c692fe3da6e4
Karthik Reddy <karthikreddylingareddy@gmail.com>
2017-12-13 21:45:17 +0530

    Update ReadLine.js

--- JS/ReadLine.js
+++ JS/ReadLine.js
@@ -1,19 +1,25 @@
 const http = require('http');
+const http = require('http');
 const fs = require('fs');
 
 const hostname = '192.168.1.10';
 const port = '2000';
 
-fs.readFile('test.txt', (err,text) => {
+fs.readFile('test.txt', (err, text) => {
 	if(err){
-		console.log(err);
+		callback(new Error('something bad happened'));
 	}
-	const server= http.createServer((req,res) => {
+	
+
+	const server= http.createServer((req, res) => {
 		res.statusCode = 200;
 		res.setHeader('Content-type','textplain');
 		res.write(text);
 		res.end();
 	});
+
+	
+
 	server.listen(port,hostname, () => {
 		console.log('Server started on port '+port);
 	});
