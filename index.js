fd9f8ba762e0e3713a23dcd60682137f735cd2d7
ankushdharkar <ankushdharkar@gmail.com>
2018-01-23 23:18:57 +0530

    Moved the end of program messsage to a new function

--- index.js
+++ index.js
@@ -8,19 +8,24 @@ function displayRows(pArrRows) {
 		if(item.length > 0){
 			console.log(item);
 		}
-		
 	});
+
+}
+
+
+function notifyEndOfProgram() {
+	// This tells us that the data reading process has completed
+	console.log('\n ===== End of Data =====');
 }
 
 
 fs.createReadStream("data/simple.csv")
 
 	.pipe(parse({delimiter: ','}))
-	
+
 	.on('data', function(arrRows) {
 		displayRows(arrRows);
 	})
 	.on('end', function() {
-		console.log('------');
-		console.log('DATA Finished');
+		notifyEndOfProgram();
 	});
