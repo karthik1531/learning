d6569c90e9026a1dc75eec7ec461ca7d3c6c2304
ankushdharkar <ankushdharkar@gmail.com>
2018-01-23 21:49:44 +0530

    Refactored the code for better readability

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 var fs = require('fs'); 
 var parse = require('csv-parse');
 
-fs.createReadStream("data/simple.csv")
-	.pipe(parse({delimiter: ','}))
-	.on('data', function(csvrow) {
 
-		for (var index = 0; index < csvrow.length; index++){
+function displayRows(pArrRows) {
 
-			if(csvrow[index].length =='0'){
-				index = index+1;
-			}
-			else{
-				console.log(csvrow[index]); 
-			}
-
+	pArrRows.forEach(function(item) {
+		if(item.length > 0){
+			console.log(item);
 		}
+		
+	});
+}
 
+
+fs.createReadStream("data/simple.csv")
+
+	.pipe(parse({delimiter: ','}))
+	
+	.on('data', function(arrRows) {
+		displayRows(arrRows);
 	})
-	.on('end',function() {
+	.on('end', function() {
 		console.log('------');
 		console.log('DATA Finished');
 	});
