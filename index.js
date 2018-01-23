b4e959d613fcb0bf72bdbe42b08761fc2a1f79b5
Your Name <--global>
2018-01-23 22:58:16 +0530

    CLeared the merge errors

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 var fs = require('fs'); 
 var parse = require('csv-parse');
 
-<<<<<<< HEAD
-fs.createReadStream("data/simple.csv")
-	.pipe(parse({delimiter: ','}))
-	.on('data', function(csvrow) {
-
-		for (var index = 0; index < csvrow.length; index++){
-			
-			if(csvrow[index].length =='0'){
-				index = index+1;
-			}
-			else{
-				console.log(csvrow[index]); }
-			}
-
-	})
-	.on('end',function() {
-=======
 
 function displayRows(pArrRows) {
 
@@ -38,7 +21,6 @@ fs.createReadStream("data/simple.csv")
 		displayRows(arrRows);
 	})
 	.on('end', function() {
->>>>>>> d6569c90e9026a1dc75eec7ec461ca7d3c6c2304
 		console.log('------');
 		console.log('DATA Finished');
 	});
