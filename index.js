29adbe8178bb15fc826bfc1066f8d092b487ddee
ankushdharkar <ankushdharkar@gmail.com>
2018-01-23 21:28:57 +0530

    Fixed some formatting

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ var fs = require('fs');
 var parse = require('csv-parse');
 
 fs.createReadStream("data/simple.csv")
-    .pipe(parse({delimiter: ','}))
-    .on('data', function(csvrow) {
-	for (var index = 0; index < csvrow.length; index++){
-	if(csvrow[index].length =='0'){
-	index = index+1;}
-	else{
-        console.log(csvrow[index]); }
-	}
-            
-    })
-    .on('end',function() {
-     console.log('------');
-     console.log('DATA Finished');
-    });
+	.pipe(parse({delimiter: ','}))
+	.on('data', function(csvrow) {
 
+		for (var index = 0; index < csvrow.length; index++){
 
+			if(csvrow[index].length =='0'){
+				index = index+1;
+			}
+			else{
+				console.log(csvrow[index]); 
+			}
+
+		}
+
+	})
+	.on('end',function() {
+		console.log('------');
+		console.log('DATA Finished');
+	});
